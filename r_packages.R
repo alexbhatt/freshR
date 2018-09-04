e32d6fdc82af6d2c18e3c13b3510507177264e96
Alex Bhattacharya <33866404+alexbhatt@users.noreply.github.com>
2018-09-04 14:50:57 +0100

    fresh_install initial commit

---
+++ r_packages.R
@@ -0,0 +1,72 @@
+
+# Base programs I want in a new R install
+# last updated: 04/09/2018
+# Alex Bhattacharya
+
+# data management
+datamgmt <- c("tidyverse", 
+              "data.table",
+              "janitor", # simple data cleaning (names, empty rows/cols; tidyverse+)
+              "nanier", # for changing any and all values to NA (tidyverse+)
+              "lubridate", # date functions (tidyverse+)
+              "skimr", # data browsing
+              "visdat" # data exploration
+              )
+install.packages(datamgmt)
+
+# epi packages
+epi <- c("epitools", # basic epi functions
+         "surveillance", # lots of surveillance functions, including exceedance algorithms
+         "epiDisplay", # havent actually used this
+         "EpiStats", ## cc/cs inter and stratification tables
+         "survival", # survival analysis
+         "zoo", # time series analysis
+         "multcomp", # glm and ci calculations
+         "Hmisc", # power and sample size calculations and lots of other things
+         "MASS", # stats package
+         "EpiCurve" # plots an epicurve
+         )
+install.packages(epi)
+
+# input of data
+inputs <- c("openxlsx", # read and write XLSX
+            "readxl", # similar to open
+            "haven", # read other data types including STATA files
+            "RODBC" # allows you to ODBC into databases
+            )
+install.packages(inputs)
+
+# graphs (ggplot included in tidyverse)
+graph <- c("cowplot", # addon to ggplot
+           "ggrepel"  # addon to ggplot
+           )
+install.packages(graph)
+
+# maps for days
+maps <- c("ggmap", # ggplot mapping
+          "leaflet", # interactive maps
+          "rgdal", # read shapefiles
+          "geojsonio" # work with jSON data
+          )
+install.packages(maps)
+
+# markdown extras
+mrkdwn <- c("htmlwidgets", # plugins
+            "kntir", # markdown essential
+            "kableExtra", # better tables for HTML markdown
+            "pander", # type conversions
+            "broom", # regression output cleanup
+            "captioner" # automate seqential figure and table numbers/captions and citations
+            ) 
+install.packages(mrkdwn)
+
+# github installs, gitlab ones require network connections
+install.packages("devtools")
+
+devtools::install_github("DanielGardiner/EpiFunc") ## agesex and epicurves
+devtools::install_git('https://gitlab.phe.gov.uk/ERD/GIS/rgisws.git', build_vignettes=TRUE) ## arc gis functionality
+devtools::install_github("PublicHealthEngland/odsR",
+                         build_vignettes = TRUE,
+                         dependencies = "suggests")
+devtools::install_git('https://gitlab.phe.gov.uk/packages/phecharts') ## PHE branding
+  
