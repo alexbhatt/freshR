3b5c8fd9bf1d82d973dcf87d816765de0ccc3616
Alex Bhattacharya <alex.bhattacharya@phe.gov.uk>
2018-11-29 10:42:18 +0000

    updated

--- r_packages.R
+++ r_packages.R
@@ -1,9 +1,9 @@
 
-# Base programs I want in a new R install
-# last updated: 04/09/2018
-# Alex Bhattacharya
+## Base programs I want in a new R install
+## last updated: 07/11/2018
+## Alex Bhattacharya
 
-# data management
+## data management
 datamgmt <- c("tidyverse", 
               "data.table",
               "janitor", # simple data cleaning (names, empty rows/cols; tidyverse+)
@@ -14,21 +14,24 @@ datamgmt <- c("tidyverse",
               )
 install.packages(datamgmt)
 
-# epi packages
+## epi/stats packages
 epi <- c("epitools", # basic epi functions
          "surveillance", # lots of surveillance functions, including exceedance algorithms
          "epiDisplay", # havent actually used this
          "EpiStats", ## cc/cs inter and stratification tables
          "survival", # survival analysis
          "zoo", # time series analysis
+         "lme4", # regression modelling
          "multcomp", # glm and ci calculations
          "Hmisc", # power and sample size calculations and lots of other things
          "MASS", # stats package
-         "EpiCurve" # plots an epicurve
+         "EpiCurve", # plots an epicurve
+         "forecast", # tsa forecasting
+         "TSA" # more time series analysis
          )
 install.packages(epi)
 
-# input of data
+## input of data
 inputs <- c("openxlsx", # read and write XLSX
             "readxl", # similar to open
             "haven", # read other data types including STATA files
@@ -36,13 +39,14 @@ inputs <- c("openxlsx", # read and write XLSX
             )
 install.packages(inputs)
 
-# graphs (ggplot included in tidyverse)
+## graphs (ggplot included in tidyverse)
 graph <- c("cowplot", # addon to ggplot
-           "ggrepel"  # addon to ggplot
+           "ggrepel",  # addon to ggplot
+           "ggseas" # time series lines w seasonality
            )
 install.packages(graph)
 
-# maps for days
+## maps for days
 maps <- c("ggmap", # ggplot mapping
           "leaflet", # interactive maps
           "rgdal", # read shapefiles
@@ -50,7 +54,7 @@ maps <- c("ggmap", # ggplot mapping
           )
 install.packages(maps)
 
-# markdown extras
+## markdown extras
 mrkdwn <- c("htmlwidgets", # plugins
             "kntir", # markdown essential
             "kableExtra", # better tables for HTML markdown
@@ -60,7 +64,9 @@ mrkdwn <- c("htmlwidgets", # plugins
             ) 
 install.packages(mrkdwn)
 
-# github installs, gitlab ones require network connections
+install.packages("tictoc") ## code runtime testing
+
+## github installs, gitlab ones require network connections
 install.packages("devtools")
 
 devtools::install_github("DanielGardiner/EpiFunc") ## agesex and epicurves
@@ -69,4 +75,7 @@ devtools::install_github("PublicHealthEngland/odsR",
                          build_vignettes = TRUE,
                          dependencies = "suggests")
 devtools::install_git('https://gitlab.phe.gov.uk/packages/phecharts') ## PHE branding
+
+devtools::install_github('ukgovdatascience/govstyle') ## GOV UK branding
+devtools::install_github("hrbrmstr/hrbrthemes") ## nice ggplot themes for graphs
   
