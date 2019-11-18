5500a7793d40ecabe9c87597b92891f844d2ed6b
Alex Bhattacharya <alex.bhattacharya@phe.gov.uk>
2019-11-18 13:43:56 +0000

    updated packages

--- r_packages.R
+++ r_packages.R
@@ -1,33 +1,45 @@
 
 ## Base programs I want in a new R install
-## last updated: 07/11/2018
+## last updated: 14/12/2018
 ## Alex Bhattacharya
 
 ## data management
-datamgmt <- c("tidyverse", 
+datamgmt <- c("tidyverse",
               "data.table",
               "janitor", # simple data cleaning (names, empty rows/cols; tidyverse+)
               "nanier", # for changing any and all values to NA (tidyverse+)
               "lubridate", # date functions (tidyverse+)
-              "skimr", # data browsing
+              "skimr", # data browsing, use!
               "visdat" # data exploration
               )
 install.packages(datamgmt)
 
+## data exploration/summary
+exp <- c("arsenal", # powerful table 1 creator
+         "tableone" # less flexible but easy to use
+         ) 
+install.packages(exp)
+
 ## epi/stats packages
 epi <- c("epitools", # basic epi functions
+         "epiR", # even more epi tools, including sample size
+         "ICC.Sample.Size", # nested and cluster sample size calculations
+         "samplingbook", # calculations for sampling
          "surveillance", # lots of surveillance functions, including exceedance algorithms
          "epiDisplay", # havent actually used this
          "EpiStats", ## cc/cs inter and stratification tables
          "survival", # survival analysis
          "zoo", # time series analysis
+         "TSA", # more time series analysis
          "lme4", # regression modelling
          "multcomp", # glm and ci calculations
          "Hmisc", # power and sample size calculations and lots of other things
          "MASS", # stats package
          "EpiCurve", # plots an epicurve
          "forecast", # tsa forecasting
-         "TSA" # more time series analysis
+         "DesignLibary", # model design
+         "fmsb", # medical stats
+         "pwr" # power sample size caluclations
          )
 install.packages(epi)
 
@@ -42,13 +54,16 @@ install.packages(inputs)
 ## graphs (ggplot included in tidyverse)
 graph <- c("cowplot", # addon to ggplot
            "ggrepel",  # addon to ggplot
-           "ggseas" # time series lines w seasonality
+           "ggseas", # time series lines w seasonality
+           "ggpubr", # plotting normality for checks
+           "survminer" # survival package curve plotting
            )
 install.packages(graph)
 
 ## maps for days
 maps <- c("ggmap", # ggplot mapping
           "leaflet", # interactive maps
+          "sf", # static maps w ggplot again
           "rgdal", # read shapefiles
           "geojsonio" # work with jSON data
           )
@@ -59,9 +74,12 @@ mrkdwn <- c("htmlwidgets", # plugins
             "kntir", # markdown essential
             "kableExtra", # better tables for HTML markdown
             "pander", # type conversions
+            "trelliscopejs", # dashboards for ggplot
+            "plotly", # interactive ggplots
             "broom", # regression output cleanup
+            "broom.mixed", # for mixed effect model output cleaning
             "captioner" # automate seqential figure and table numbers/captions and citations
-            ) 
+            )
 install.packages(mrkdwn)
 
 install.packages("tictoc") ## code runtime testing
@@ -69,7 +87,9 @@ install.packages("tictoc") ## code runtime testing
 ## github installs, gitlab ones require network connections
 install.packages("devtools")
 
-devtools::install_github("DanielGardiner/EpiFunc") ## agesex and epicurves
+httr::set_config(httr::config(ssl_verifypeer=0L))
+
+devtools::install_github("DanielGardiner/EpiFunc", force=T) ## agesex and epicurves
 devtools::install_git('https://gitlab.phe.gov.uk/ERD/GIS/rgisws.git', build_vignettes=TRUE) ## arc gis functionality
 devtools::install_github("PublicHealthEngland/odsR",
                          build_vignettes = TRUE,
@@ -78,4 +98,6 @@ devtools::install_git('https://gitlab.phe.gov.uk/packages/phecharts') ## PHE bra
 
 devtools::install_github('ukgovdatascience/govstyle') ## GOV UK branding
 devtools::install_github("hrbrmstr/hrbrthemes") ## nice ggplot themes for graphs
-  
+
+devtools::install_github("hrbrmstr/nominatim") ## for working with openstreet map geocoding
+
